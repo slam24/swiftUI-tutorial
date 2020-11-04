090fd3fb852c6758c709faf32453d8279b78cece
slam24 <luismartinez242011@gmail.com>
2020-11-04 12:19:58 -0800

    initial commit

---
+++ Helloworld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Helloworld
+//
+//  Created by slam24 on 11/4/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Helloworld/HelloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloworldApp.swift
+//  Helloworld
+//
+//  Created by slam24 on 11/4/20.
+//
+
+import SwiftUI
+
+@main
+struct HelloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
