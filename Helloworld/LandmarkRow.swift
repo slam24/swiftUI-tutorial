3cf39f2c957de8203015a5ee28859d5a8f714022
Luis Alfredo Martínez Sánchez <luismartinez242011@gmail.com>
2020-11-06 14:22:13 -0800

    feat: Get to Know the Sample Data

---
+++ Helloworld/LandmarkRow.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkRow.swift
+//  Helloworld
+//
+//  Created by slam24 on 11/6/20.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow()
+    }
+}
